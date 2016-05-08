6f1a4a9e0a63abb94e338a411bdaeec5bd1e613e
chis <chis@oupeng.com>
2016-05-08 22:37:59 +0800

    test

--- src/app/client/containers/Root.js
+++ src/app/client/containers/Root.js
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from 'react'
-import { Provider } from 'react-redux'
-import routes from '../../routes'
-import { ReduxRouter } from 'redux-router'
+import React, { Component, PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import routes from '../../routes';
+import { ReduxRouter } from 'redux-router';
 
 export default class Root extends Component {
   render() {
