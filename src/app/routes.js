89cbf1b39f52a3a405886ad1ab80b090b1d1256f
miaomiao8890 <miaomiao8890@vip.qq.com>
2016-05-09 19:44:56 +0800

    test

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,5 +4,11 @@ import App from './client/containers/App.js'
 
 export default (
   <Route path="/"  component={App}>
+  	{ /* Routes requiring login */ }
+    <Route onEnter={requireLogin}>
+      <Route path="login" component={Login}/>
+    </Route>
+    { /* Catch all route */ }
+    <Route path="*" component={NotFound} status={404} />
   </Route>
 )
